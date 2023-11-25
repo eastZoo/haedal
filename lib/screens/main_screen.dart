f3af84819db54a917c55f45b549b1615c661e275
신동주 <eastzoo@eastzoo.local>
2023-11-25 17:47:44 +0900

    merge

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -21,7 +21,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
-  int _selectedIndex = 1;
+  int _selectedIndex = 2;
   final autoSizeGroup = AutoSizeGroup();
 
   final iconList = [
