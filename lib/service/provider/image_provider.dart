b0e3f72aa7dd10bd9fccb6b2bc5c70a7e0a35c1a
신동주 <eastzoo@eastzoo.local>
2023-11-25 16:35:35 +0900

    develop branch initial

--- lib/service/provider/image_provider.dart
+++ lib/service/provider/image_provider.dart
@@ -2,7 +2,7 @@ import 'package:haedal/service/api_request.dart';
 import 'package:haedal/service/endpoints.dart';
 
 class ImagesProvider {
-  // 이미지 분석 전송 버튼
+  // // 이미지 분석 전송 버튼
   create(data) async {
     return await ApiRequest(url: Endpoints.imageUrl, data: data).formPost();
   }
