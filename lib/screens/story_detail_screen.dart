9a617a55d82e9c7f639c18aaf7ca619098c7bc7f
eastzoo <dongju010@kakao.com>
2023-12-09 13:23:23 +0900

    UPDATE : 스토리 상세 페이지 앱바, 리스트 레이아웃 초기 설정

--- lib/screens/story_detail_screen.dart
+++ lib/screens/story_detail_screen.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:haedal/models/album.dart';
 import 'package:haedal/styles/colors.dart';
 import '../../service/endpoints.dart';
+import 'package:haedal/styles/colors.dart';
 
 class StoryDetailScreen extends StatefulWidget {
   const StoryDetailScreen({super.key, this.albumBoard});
@@ -20,40 +21,129 @@ class _StoryDetailScreenState extends State<StoryDetailScreen> {
   _StoryDetailScreenState(this.albumBoard);
   AlbumBoard albumBoard;
 
+  final ScrollController _scrollController = ScrollController();
+  double position = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    // Add a listener to the ScrollController
+    _scrollController.addListener(_scrollListener);
+  }
+
+  // Dispose of the ScrollController when the widget is disposed
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  // Scroll listener callback
+  void _scrollListener() {
+    // Access the scroll position using _scrollController.position.pixels
+    double scrollPosition = _scrollController.position.pixels;
+    setState(() {
+      position = scrollPosition;
+    });
+    print('Scroll Position: $scrollPosition');
+  }
+
   @override
   Widget build(BuildContext context) {
     print("albunm!!!! : $albumBoard");
+
     return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        centerTitle: true,
-        elevation: 0,
-        leading: const BackButton(
-          color: Colors.white,
-        ),
-        actions: [_addBtn()],
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            _thumbPhoto(),
-            _thumbPhoto(),
-            _thumbPhoto(),
-          ],
+      body: NestedScrollView(
+        controller: _scrollController,
+        floatHeaderSlivers: true,
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          SliverAppBar(
+            automaticallyImplyLeading: true,
+            expandedHeight: 240,
+            flexibleSpace: FlexibleSpaceBar(
+              background: _thumbPhoto(),
+            ),
+            actions: [_addBtn(position)],
+            leading: BackButton(
+              color: position < 150 ? Colors.white : AppColors().mainColor,
+            ),
+            title: position < 150
+                ? null
+                : AnimatedBuilder(
+                    animation: _scrollController,
+                    builder: (BuildContext context, Widget? child) {
+                      double offset = _scrollController.hasClients
+                          ? _scrollController.offset
+                          : _scrollController.initialScrollOffset;
+
+                      // Calculate the title position based on the scroll offset
+                      double titlePosition = 100.0 - (offset - 180);
+
+                      return Container(
+                        padding: const EdgeInsets.only(bottom: 16.0),
+                        child: Transform.translate(
+                          offset: Offset(0, titlePosition),
+                          child: Text(
+                            'Scrolling Title',
+                            style: TextStyle(
+                                fontSize: 17, color: AppColors().mainColor),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+            centerTitle: true,
+            pinned: true,
+          ),
+        ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              _thumbPhoto(),
+              _thumbPhoto(),
+              _thumbPhoto(),
+              _thumbPhoto(),
+              _thumbPhoto(),
+              _thumbPhoto(),
+              _thumbPhoto(),
+            ],
+          ),
         ),
       ),
     );
+    // return Scaffold(
+    //   extendBodyBehindAppBar: true,
+    //   appBar: AppBar(
+    //     backgroundColor: Colors.transparent,
+    //     centerTitle: true,
+    //     elevation: 0,
+    //     leading: const BackButton(
+    //       color: Colors.white,
+    //     ),
+    //     actions: [_addBtn()],
+    //   ),
+    //   body: SingleChildScrollView(
+    //     child: Column(
+    //       children: [
+    //         _thumbPhoto(),
+    //         _thumbPhoto(),
+    //         _thumbPhoto(),
+    //       ],
+    //     ),
+    //   ),
+    // );
   }
 
-  Widget _addBtn() {
+  Widget _addBtn(double position) {
     return Padding(
       padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
       child: GestureDetector(
-        child: const Icon(
+        child: Icon(
           Icons.add,
-          size: 50,
-          color: Colors.white,
+          size: 25,
+          color: position < 150 ? Colors.white : AppColors().mainColor,
         ),
         onTap: () async {},
       ),
@@ -83,9 +173,9 @@ class _StoryDetailScreenState extends State<StoryDetailScreen> {
           child: Container(
             alignment: Alignment.center,
             color: Colors.grey.withOpacity(0.2),
-            child: const Text(
-              "title",
-              style: TextStyle(
+            child: Text(
+              "${albumBoard.title}",
+              style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
                 color: Colors.white,
