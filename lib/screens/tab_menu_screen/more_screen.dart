36e12048127250baaa0349d93d8de6b09ffe0592
동주 <67012957+eastZoo@users.noreply.github.com>
2024-05-30 11:34:58 +0900

    UPDATE : memo화면 테스트 UI

--- lib/screens/tab_menu_screen/more_screen.dart
+++ lib/screens/tab_menu_screen/more_screen.dart
@@ -7,34 +7,69 @@ class MoreScreen extends StatefulWidget {
   const MoreScreen({super.key});
 
   @override
-  State<MoreScreen> createState() => _MoreScreenState();
+  _MoreScreenState createState() => _MoreScreenState();
 }
 
 class _MoreScreenState extends State<MoreScreen> {
-  void logOut() {
-    const storage = FlutterSecureStorage();
-    storage.delete(key: "accessToken");
-
-    Navigator.pushNamedAndRemoveUntil(context, "/login", (route) => false);
-  }
+  bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 500),
+            height: isExpanded ? 0 : MediaQuery.of(context).size.height / 3,
+            child: Container(
+              color: Colors.blue,
+              child: const Center(
+                  child: Text('A 화면',
+                      style: TextStyle(color: Colors.white, fontSize: 24))),
+            ),
+          ),
           Expanded(
-              child: Column(
-            children: [
-              MyButton(
-                key: const ValueKey("근무표"),
-                onTap: () {
-                  logOut();
-                },
-                title: '로그아웃',
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isExpanded = !isExpanded;
+                });
+              },
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 500),
+                height: isExpanded
+                    ? MediaQuery.of(context).size.height
+                    : MediaQuery.of(context).size.height / 3,
+                decoration: const BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    ListView.builder(
+                      itemCount: 30, // 예시로 30개의 아이템을 생성
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          title: Text('Item $index'),
+                        );
+                      },
+                    ),
+                    Align(
+                      alignment: Alignment.topCenter,
+                      child: Icon(
+                        isExpanded ? Icons.arrow_downward : Icons.arrow_upward,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ],
-          ))
+            ),
+          ),
         ],
       ),
     );
