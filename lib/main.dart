596605736ae7c26cd022205dda8098aaecead8f3
동주 <67012957+eastZoo@users.noreply.github.com>
2024-06-03 17:15:57 +0900

    FEAT : flutter_screenutil 스크린 main 적용테스트

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,13 @@ import 'package:haedal/routes/app_pages.dart';
 import 'package:haedal/styles/colors.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
 
-// 테스트 앱키 ( 동의항목 전체 오픈 )
+  // 테스트 앱키 ( 동의항목 전체 오픈 )
   KakaoSdk.init(nativeAppKey: '${dotenv.env['KAKAO_NATIVE_API_KEY']}');
   await NaverMapSdk.instance.initialize(
       clientId: '${dotenv.env['NAVER_CLIENT_ID']}',
@@ -32,33 +33,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
 
-    return GetMaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            fontFamily: 'Pretendard', primaryColor: AppColors().mainColor),
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        supportedLocales: const [
-          Locale('en', ''), // English, no country code
-          Locale('ko', ''), // Korean, no country code
-        ],
-        locale: const Locale('ko'),
-        enableLog: true,
-        logWriterCallback: (String text, {bool isError = false}) {
-          Future.microtask(() => print('** $text. isError: [$isError]'));
-        },
-        initialRoute: "/splash",
-        getPages: AppPages.routes,
-        builder: (context, child) {
-          child = EasyLoading.init()(context, child);
-          return MediaQuery(
-            data: MediaQuery.of(context)
-                .copyWith(textScaler: const TextScaler.linear(1.0)),
-            child: child,
-          );
-        });
+    return ScreenUtilInit(
+      designSize: const Size(390, 844),
+      builder: (context, child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+              fontFamily: 'Pretendard', primaryColor: AppColors().mainColor),
+          localizationsDelegates: const [
+            GlobalMaterialLocalizations.delegate,
+            GlobalWidgetsLocalizations.delegate,
+            GlobalCupertinoLocalizations.delegate,
+          ],
+          supportedLocales: const [
+            Locale('en', ''), // English, no country code
+            Locale('ko', ''), // Korean, no country code
+          ],
+          locale: const Locale('ko'),
+          enableLog: true,
+          logWriterCallback: (String text, {bool isError = false}) {
+            Future.microtask(() => print('** $text. isError: [$isError]'));
+          },
+          initialRoute: "/splash",
+          getPages: AppPages.routes,
+          builder: (context, child) {
+            child = EasyLoading.init()(context, child);
+            return MediaQuery(
+              data: MediaQuery.of(context)
+                  .copyWith(textScaler: const TextScaler.linear(1.0)),
+              child: child,
+            );
+          },
+        );
+      },
+    );
   }
 }
