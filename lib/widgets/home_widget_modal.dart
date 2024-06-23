e7ef08ed6964a4d34d968a426ee2dd622d7bd6e1
eastZoo <dongju010@naver.com>
2024-06-23 23:32:07 +0900

    UPDATE : 홈화면 꾸미기 위젯 첫번쨰 추가

--- lib/widgets/home_widget_modal.dart
+++ lib/widgets/home_widget_modal.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:gap/gap.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:haedal/service/controller/home_controller.dart';
+import 'package:haedal/styles/colors.dart';
 import 'package:haedal/widgets/home_widget_sample.dart';
 
 class HomeWidgetModal extends StatelessWidget {
@@ -34,6 +35,7 @@ class HomeWidgetModal extends StatelessWidget {
                     ),
                   ),
                 ),
+                const Gap(10),
                 Expanded(
                   child: GridView.builder(
                     padding: const EdgeInsets.all(10),
@@ -42,7 +44,7 @@ class HomeWidgetModal extends StatelessWidget {
                       crossAxisCount: 3, // 3열
                       mainAxisSpacing: 10,
                       crossAxisSpacing: 10,
-                      childAspectRatio: 2, // 아이템의 가로 세로 비율
+                      childAspectRatio: 1, // 아이템의 가로 세로 비율
                     ),
                     itemCount: 9, // 아이템 수
                     itemBuilder: (BuildContext context, int index) {
@@ -56,17 +58,10 @@ class HomeWidgetModal extends StatelessWidget {
                         child: Container(
                           alignment: Alignment.center,
                           decoration: BoxDecoration(
-                            color: Colors.blue,
+                            color: Colors.black.withOpacity(0.2),
                             borderRadius: BorderRadius.circular(10),
                           ),
-                          child: Container(
-                            alignment: Alignment.center,
-                            decoration: BoxDecoration(
-                              color: Colors.blue,
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                            child: home_widget_sample(index), // index에 따라 위젯 선택
-                          ),
+                          child: home_widget_sample(index), // index에 따라 위젯 선택
                         ),
                       );
                     },
--- lib/widgets/home_widget_sample.dart
+++ lib/widgets/home_widget_sample.dart
@@ -3,9 +3,91 @@ import 'package:flutter/material.dart';
 Widget home_widget_sample(int index) {
   switch (index) {
     case 0:
-      return const Text(
-        'Add Item 1',
-        style: TextStyle(color: Colors.white),
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.transparent,
+            ),
+          ),
+          child: const Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "2024-06-23",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 14,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 9.0,
+                      color: Colors.black54,
+                      offset: Offset(1.0, 1.5),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 4),
+              Text(
+                "365일",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 25,
+                  shadows: [
+                    Shadow(
+                      blurRadius: 9.0,
+                      color: Colors.black54,
+                      offset: Offset(1.0, 1.5),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 4),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    "해달",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 9.0,
+                          color: Colors.black54,
+                          offset: Offset(1.0, 1.5),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 2),
+                  Icon(Icons.favorite, color: Colors.white, size: 14),
+                  SizedBox(width: 2),
+                  Text(
+                    "수달",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 14,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 9.0,
+                          color: Colors.black54,
+                          offset: Offset(1.0, 1.5),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       );
     case 1:
       return const Text(
